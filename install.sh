c511ad0f63934ea80f9c4cec84f723826584e44d
YuZhong-Chen <yuzhong1214@gmail.com>
2023-10-14 14:55:21 +0800

    Update install.sh for zsh path.

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 # zshrc setting
 if [ $# = 1 ]; then
     if [ $1 = "ros1" ]; then
-        sed -i '1 i # Script setting\nexport ZSH_SCRIPT_SETTING="ros1"\n' ./zshrc/.zshrc
+        sed -i '1 i # Script setting\nexport ZSH_SCRIPT_SETTING="ros1"\n' ./zsh_config/.zshrc
     elif [ $1 = "ros2" ]; then
-        sed -i '1 i # Script setting\nexport ZSH_SCRIPT_SETTING="ros2"\n' ./zshrc/.zshrc
+        sed -i '1 i # Script setting\nexport ZSH_SCRIPT_SETTING="ros2"\n' ./zsh_config/.zshrc
     fi
 else 
-    sed -i '1 i # Script setting\nexport ZSH_SCRIPT_SETTING=""\n' ./zshrc/.zshrc
+    sed -i '1 i # Script setting\nexport ZSH_SCRIPT_SETTING=""\n' ./zsh_config/.zshrc
 fi
 
 # Update package list
@@ -20,9 +20,9 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 mkdir -p ~/.oh-my-zsh/custom/plugins/jovial
-cp ./zshrc/.zshrc ~/
-cp ./zsh_theme/jovial.zsh-theme ~/.oh-my-zsh/custom/themes/jovial.zsh-theme
-cp ./zsh_theme/jovial.plugin.zsh ~/.oh-my-zsh/custom/plugins/jovial/jovial.plugin.zsh
+cp ./zsh_config/.zshrc ~/
+cp ./zsh_config/jovial.zsh-theme ~/.oh-my-zsh/custom/themes/jovial.zsh-theme
+cp ./zsh_config/jovial.plugin.zsh ~/.oh-my-zsh/custom/plugins/jovial/jovial.plugin.zsh
 sudo chsh -s /bin/zsh
 
 # Install tmux
